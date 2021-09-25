4c0119536de33fa2050443776e3ab72172e5ef1b
erwanclx <81480765+erwanclx@users.noreply.github.com>
2021-09-25 10:17:22 +0200

    Add scroll solution

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', scroll);
+
+function scroll() {
+  if (window.scrollY > 900) {
+  navbar.classList.add("background");
+  }
+  if (window.scrollY < 900) {
+    navbar.classList.remove("background");
+    }
+}
+  
+
 var typed = new Typed('#typed',{
     stringsElement: '#typed-strings',
     backSpeed: 40,
