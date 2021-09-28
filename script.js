9117731a6b4af2e523c01b4145c0d854732deba3
erwanclx <81480765+erwanclx@users.noreply.github.com>
2021-09-28 19:52:24 +0200

    change attitude with scroll

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-let navbar = document.querySelector('.navbar');
+let navbarlogo = document.querySelector('.alogo');
+let li = document.querySelectorAll('.linav');
+let pages = document.querySelectorAll("section");
+let spanlogo = document.querySelectorAll(".colorlogo");
+let options = { threshold: 0 };
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          navbarlogo.classList.add("backgroundlogo");
+          navbarlogo.classList.add("transition");
+          li.forEach(element => {
+            element.classList.add("backgroundli");
+            element.classList.add("transition");
+          });
+          spanlogo.forEach(element => {
+            element.classList.add("colorlogoscroll");
+            element.classList.add("transition");
+          });
+        }
+        else {
+          navbarlogo.classList.add("transition");
+          navbarlogo.classList.remove("backgroundlogo"); 
+          li.forEach(element => {
+            element.classList.remove("backgroundli");
+            element.classList.add("transition");
+          }); 
+          spanlogo.forEach(element => {
+            element.classList.remove("colorlogoscroll");
+            element.classList.add("transition");
+          });
+        }
+    });
+}, options);
 
-window.addEventListener('scroll', scroll);
-
-function scroll() {
-  if (window.scrollY > 900) {
-  navbar.classList.add("background");
-  }
-  if (window.scrollY < 900) {
-    navbar.classList.remove("background");
-    }
-}
-  
+[...pages].forEach((element) => observer.observe(element));
 
 var typed = new Typed('#typed',{
     stringsElement: '#typed-strings',
