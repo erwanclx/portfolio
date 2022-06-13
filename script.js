1d4b79a266bfb98200bcc5ac147f27f58968be5e
Erwan <erwanclou@gmail.com>
2022-06-13 18:26:47 +0200

    card home + vertical nav

--- script.js
+++ script.js
@@ -1,42 +1,85 @@
-let navbarlogo = document.querySelector('.alogo');
-let li = document.querySelectorAll('.linav');
-let pages = document.querySelectorAll("section");
-let spanlogo = document.querySelectorAll(".colorlogo");
-let options = { threshold: 0 };
-let observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          navbarlogo.classList.add("backgroundlogo");
-          navbarlogo.classList.add("transition");
-          li.forEach(element => {
-            element.classList.add("backgroundli");
-            element.classList.add("transition");
-          });
-          spanlogo.forEach(element => {
-            element.classList.add("colorlogoscroll");
-            element.classList.add("transition");
-          });
-        }
-        else {
-          navbarlogo.classList.add("transition");
-          navbarlogo.classList.remove("backgroundlogo"); 
-          li.forEach(element => {
-            element.classList.remove("backgroundli");
-            element.classList.add("transition");
-          }); 
-          spanlogo.forEach(element => {
-            element.classList.remove("colorlogoscroll");
-            element.classList.add("transition");
-          });
-        }
-    });
-}, options);
-
-[...pages].forEach((element) => observer.observe(element));
-
 var typed = new Typed('#typed',{
     stringsElement: '#typed-strings',
     backSpeed: 40,
     typeSpeed: 40,
     loop: true
   });
+
+  const sections = [...document.querySelectorAll(".content-section")]
+const navLinks = [...document.querySelectorAll("nav a")]
+const documentationContainer = document.querySelector(".documentation-content")
+
+window.addEventListener("load", () => {
+  let data = sections.map(section => section.offsetTop)
+  console.log(data);
+
+  // Resize Observer
+  const resizeObserver = new ResizeObserver(handleResize)
+  resizeObserver.observe(documentationContainer)
+
+  let firstLoad = true;
+  function handleResize(){
+    if(!firstLoad)  data = sections.map(section => section.offsetTop)
+    firstLoad = false;
+  }
+
+  // Intersection Observer
+  const intersectionObserver = new IntersectionObserver(startWatching, {rootMargin: "10% 0px"}) 
+  intersectionObserver.observe(documentationContainer)
+
+  function startWatching(entries) {
+    if(entries[0].isIntersecting) {
+      window.addEventListener("scroll", handleScroll)
+    } else if (!entries[0].isIntersecting) {
+      const elToClean = navLinks.find(navLink => navLink.className.includes("marked"))
+      if(elToClean) elToClean.classList.remove("marked")
+      savedIndex = undefined;
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }
+
+  navLinks.forEach((navLink, index) => {
+    navLink.addEventListener("click", e => {
+      e.preventDefault()
+
+      window.scrollTo({
+        top: data[index],
+        behavior: "smooth"
+      })
+    })
+  })
+
+  // window.addEventListener("scroll", handleScroll)
+
+  let savedIndex;
+  function handleScroll(){
+
+    const trigger = window.scrollY + (window.innerHeight/2)
+
+    for(const i of data) {
+      const index = data.indexOf(i)
+
+      if(trigger >= data[index] && trigger < data[index +1]){
+        if(index !== savedIndex) {
+          savedIndex = index;
+          addClassAndClear(index)
+        }
+        break;
+      }
+
+      if(index === data.length - 1 && trigger >= data[index]) {
+        if(index !== savedIndex) {
+          savedIndex = index;
+          addClassAndClear(index)
+        }
+      }
+    }
+  }
+  handleScroll()
+
+  function addClassAndClear(index) {
+    const elToClean = navLinks.find(navLink => navLink.className.includes("marked"))
+    if(elToClean) elToClean.classList.remove("marked")
+    navLinks[index].classList.add("marked")
+  }
+})
--- slides.js
+++ slides.js
@@ -1,28 +1,53 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+function requestUserRepos(username){
+  const xhr = new XMLHttpRequest();
+  const url = `https://api.github.com/users/${username}/repos?sort=created&order=asc`;
+  xhr.open('GET', url, true);
+  xhr.onload = function() {
+      const data = JSON.parse(this.response);
+      for(let i in data) {
+        var container = document.getElementById('card-proj');
+        console.log(data[i].name);
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+        <div class="card proj-img">
+        <div class="overlay">
+          <h4 class="overlay-title">${data[i].name}</h4>
+            <svg class="card_arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+            <a class="download" href="https://github.com/erwanclx/" target="_blank">Voir le code <i class="fas fa-arrow-right"></i></a>
+        </div>
+      </div>
+                  `;
+       container.appendChild(card);
+      }
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  xhr.send();  
+}
+requestUserRepos('erwanclx');
+const buttonRight = document.querySelector("#btn-scroll-right");
+const buttonLeft = document.querySelector("#btn-scroll-left");
+cardproj = document.querySelector('#card-proj');
+// buttonRight.onclick = function () {
+//   document.getElementById('card-proj').scrollLeft += 400;
+// };
+// buttonLeft.onclick = function () {
+//   document.getElementById('card-proj').scrollLeft -= 400;
+// };
+function scrollLefts(){
+  cardproj.scrollLeft += 400
+  };
+
+function scrollRight(){
+  cardproj.scrollLeft -=400
+};
+
+  buttonLeft.addEventListener('click', scrollRight)
+  buttonRight.addEventListener('click', scrollLefts)
+  
+  // if(cardproj.scrollLeft == 0){
+  // buttonLeft.classList.add('opacity-0-css-class')
+  // }
+  
+  if(cardproj.scrollLeft !== 0){
+  buttonLeft.classList.remove('opacity-0-css-class');
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
